bdd0433a6bcfdd98c9e7707687c90c9be50ac07c
pratyaksh <pratyaksh22csu447@ncuindia.edu>
2024-10-16 00:30:28 +0530

    added cors

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
-
-const app = express();
-app.use(express.json())
-const PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-
-
+//router imports
 const DonationRouter = require("./router/DonationRouter");
 const AuthenticationRouter = require("./router/AuthenticationRouter");
 
+//port
+const PORT = process.env.PORT || 3000;
+
+//create the app
+const app = express();
+
+//middlewares
+app.use(express.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 
+
+//router
 app.use("/auth",AuthenticationRouter);
 app.use(DonationRouter);
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
